f76540215296201d920cfd60e280ba17dbdacfa9
baryshevvet <baryshev.vet@gmail.com>
2023-03-01 12:39:36 -0800

    introduced mockgen, mock objects, refactored tests

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -6,13 +6,20 @@ import (
 	"fmt"
 )
 
-type Store struct {
-	*Queries
-	db *sql.DB
+// Store provides all functions to execute db queries and transactions.
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
 }
 
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+// SQLStore provides all functions to execute SQL queries and transactions.
+type SQLStore struct {
+	db *sql.DB
+	*Queries
+}
+
+func NewStore(db *sql.DB) *SQLStore {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
@@ -36,7 +43,7 @@ type TransferTxResult struct {
 
 // TransferTx performs a money transfer from one account to the other.
 // It creates a transfer record, add account entries and update accounts' balance within a single database transaction
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	execErr := store.execTx(ctx, func(q *Queries) error {
@@ -79,7 +86,7 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 }
 
 // execTx executes a function within a database transaction
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
 		return nil
